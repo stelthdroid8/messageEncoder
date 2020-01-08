945daeed748151463916045cee58c75fd9b4c267
stelthdroid8 <brandon.kennedy1994@gmail.com>
2020-01-07 21:27:37 -0500

    0 out margin of display message

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
+const { hash } = window.location;
+
+const decodedMessage = atob(hash.replace("#", ""));
+if (decodedMessage) {
+  document.querySelector("#input-form").classList.add("hide");
+
+  document.querySelector("#message-show").classList.remove("hide");
+
+  document.querySelector("#decoded-message").innerHTML = decodedMessage;
+}
 document.querySelector("form").addEventListener("submit", event => {
   event.preventDefault();
 
-  const input = document.querySelector("input");
+  const input = document.querySelector("#message-input");
+  const encrypted = btoa(input.value);
+  document.querySelector("#input-form").classList.add("hide");
+  document.querySelector("#link").classList.remove("hide");
+  const link = document.querySelector("#link-input");
+  link.value = `${window.location}#${encrypted}`;
+  link.select();
+  //   link.value = encrypted;
 });
